e7411b54fd7f9b234646c4d8d827043a261b2aeb
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2024-08-15 22:10:23 +0700

    zig: set up required data

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,5 +1,37 @@
 // raylib-zig (c) Nikolas Wipper 2023
 
+// 35 opcodes, all two bytes long.
+const Opcode = enum(u16) {};
+
+// NOTE: Data can be organized into an array later.
+
+// 4KB memory.
+// Memory mapping.
+// 0x000-0x1FF Reserved for interpreter.
+// 0x200-0xFFF ROM & RAM.
+var memory: [4096]u8 = undefined;
+
+// 16 8-bit data registers (V0-VF).
+// VF is used as flag for some instructions, should be avoided.
+var V: [16]u8 = undefined;
+
+// Address register.
+var I: u16 = undefined;
+
+// Program counter.
+var PC: u16 = undefined;
+
+// Timers.
+var delay_timer: u8 = undefined;
+var sound_timer: u8 = undefined;
+
+// Stack for subroutine jumping.
+var stack: [16]u16 = undefined;
+var sp: u16 = undefined;
+
+// Keypad keys.
+var key: [16]u16 = undefined;
+
 const rl = @import("raylib");
 
 pub fn main() anyerror!void {
